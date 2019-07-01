80d23a4ed9ef162612116315024028b198e92151
zhivko <klemen.zivkovic@gmail.com>
2019-07-01 15:00:48 +0200

    Update taskmanager.cpp

--- main/taskmanager.cpp
+++ main/taskmanager.cpp
@@ -1,102 +1,35 @@
 #include "Taskmanager.h"
 #include <esp_task_wdt.h>
 
-void taskmanageTask(void * pvParameters) {
-	TaskStatus_t *pxTaskStatusArray = nullptr;
-	volatile UBaseType_t uxArraySize;
-	uint32_t ulTotalRunTime;
-	char outputBuffer[256];
+void taskmanageTask(void *params) {
+  while (true) {
+    UBaseType_t taskCount = uxTaskGetNumberOfTasks();
+    std::unique_ptr<TaskStatus_t[]> taskList(new TaskStatus_t[taskCount]);
+    uint32_t ulTotalRunTime;
+    UBaseType_t retrievedTaskCount = uxTaskGetSystemState(taskList.get(), taskCount, &ulTotalRunTime);
+    printf("time[us]: %" PRIu64 " core: %d, freeHeap: %u, largest: %u\n",
+           esp_timer_get_time(), xPortGetCoreID(), heap_caps_get_free_size(MALLOC_CAP_INTERNAL),
+           heap_caps_get_largest_free_block(MALLOC_CAP_8BIT));
 
-	while (true) {
-		uxArraySize = uxTaskGetNumberOfTasks();
+    printf("%15s%10s%10s%10s%10s%10s%10s%10s%10s\n",
+           "NAME", "ID", "STATE", "PRIO", "BASE", "TIME", "CPU", "STACK", "CORE");
 
-		if (pxTaskStatusArray != nullptr) //Free memory
-			delete pxTaskStatusArray;
-
-		pxTaskStatusArray = (TaskStatus_t*) pvPortMalloc(
-				uxArraySize * sizeof(TaskStatus_t));
-		uxArraySize = uxTaskGetSystemState(pxTaskStatusArray, uxArraySize,
-				&ulTotalRunTime);
-
-		size_t largest = heap_caps_get_largest_free_block(MALLOC_CAP_8BIT);
-
-		Serial.printf(
-				"time[s]: %" PRIu64 " uptime[h]: %.2f core: %d, freeHeap: %u, largest: %u\n",
-				mySecond, timeH, xPortGetCoreID(), freeheap, largest);
-
-		Serial.printf("%15s%10s%10s%10s%10s%10s%10s%10s%10s\r\n", "NAME", "ID",
-				"STATE", "PRIO", "BASE", "TIME", "CPU", "STACK", "CORE");
-
-		for (int i = 0; i < uxArraySize; i++) {
-
-			/*
-			 vTaskGetInfo(
-			 pxTaskStatusArray[i].xHandle,
-			 &xTaskDetails,
-			 pdTRUE,
-			 eInvalid );
-			 uint32_t taskWatermark = xTaskDetails.usStackHighWaterMark;
-			 ((tskTCB*)pxTaskStatusArray[i].xHandle)-
-
-			 pxTaskStatusArray[i].
-			 */
-
-			sprintf(outputBuffer, "%15s", pxTaskStatusArray[i].pcTaskName);
-			Serial.print(outputBuffer);
-			sprintf(outputBuffer, "%10u", pxTaskStatusArray[i].xTaskNumber);
-			Serial.print(outputBuffer);
-
-			switch (pxTaskStatusArray[i].eCurrentState) {
-			case eRunning:
-				sprintf(outputBuffer, "%10s", "running");
-				Serial.print(outputBuffer);
-				break;
-			case eReady:
-				sprintf(outputBuffer, "%10s", "ready");
-				Serial.print(outputBuffer);
-				break;
-			case eBlocked:
-				sprintf(outputBuffer, "%10s", "blocked");
-				Serial.print(outputBuffer);
-				break;
-			case eSuspended:
-				sprintf(outputBuffer, "%10s", "suspended");
-				Serial.print(outputBuffer);
-				break;
-			case eDeleted:
-				sprintf(outputBuffer, "%10s", "deleted");
-				Serial.print(outputBuffer);
-				break;
-			default:
-				sprintf(outputBuffer, "%10s", "unknown");
-				Serial.print(outputBuffer);
-				break;
-			}
-
-			sprintf(outputBuffer, "%10u",
-					pxTaskStatusArray[i].uxCurrentPriority);
-			Serial.print(outputBuffer);
-			sprintf(outputBuffer, "%10u", pxTaskStatusArray[i].uxBasePriority);
-			Serial.print(outputBuffer);
-			sprintf(outputBuffer, "%10u",
-					pxTaskStatusArray[i].ulRunTimeCounter);
-			Serial.print(outputBuffer);
-			sprintf(outputBuffer, "%10f",
-					(float) pxTaskStatusArray[i].ulRunTimeCounter
-							/ (float) ulTotalRunTime);
-			Serial.print(outputBuffer);
-			sprintf(outputBuffer, "%10d",
-					pxTaskStatusArray[i].usStackHighWaterMark);
-			Serial.print(outputBuffer);
-			sprintf(outputBuffer, "%10d", pxTaskStatusArray[i].xCoreID);
-			Serial.print(outputBuffer);
-			sprintf(outputBuffer, "\r\n");
-			Serial.print(outputBuffer);
-		}
-		esp_err_t err = esp_task_wdt_reset();
-		if (err != ESP_OK) {
-			log_e("Failed to feed WDT! Error: %d", err);
-		}
-		delay(20000);
-	}
+    for (int task = 0; task < retrievedTaskCount; task++) {
+      printf("%15s%10d%10s%10d%10d%10ull%10.2f%10d%10d\n",
+             taskList[task].pcTaskName,
+             taskList[task].xTaskNumber,
+             taskList[task].eCurrentState == eRunning ? "Running" : 
+               taskList[task].eCurrentState == eReady ? "Ready" : 
+               taskList[task].eCurrentState == eBlocked ? "Blocked" : 
+               taskList[task].eCurrentState == eSuspended ? "Suspended" : 
+               taskList[task].eCurrentState == eDeleted ? "Deleted" : "Unknown",
+             taskList[task].uxCurrentPriority,
+             taskList[task].uxBasePriority,
+             taskList[task].ulRunTimeCounter,
+             (float)taskList[task].ulRunTimeCounter / (float)ulTotalRunTime,
+             taskList[task].usStackHighWaterMark,
+             taskList[task].xCoreID);
+    }
+    vTaskDelay(pdMS_TO_TICKS(20000));
+  }
 }
